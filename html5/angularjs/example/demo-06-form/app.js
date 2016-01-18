b142df1ad8aa8150975104f574046ea682bbdb11
Yohann <yhui02@gmail.com>
2016-01-18 10:15:33 +0800

    angularjs 相关

--- html5/angularjs/example/demo-06-form/app.js
+++ html5/angularjs/example/demo-06-form/app.js
@@ -1,9 +1,35 @@
 angular.module('app', [])
-.controller('AddUserController', function($scope) {
-    $scope.message = '';
-    $scope.addUser = function () {
-        // TODO for the reader: actually save user to database...
-        console.log($scope.user);
-        $scope.message = 'Thanks, ' + $scope.user.first + ', we added you!';
-    };
-});
+    .controller('AddUserController', function ($scope) {
+        $scope.message = '';
+        $scope.addUser = function () {
+            // TODO for the reader: actually save user to database...
+            console.log($scope.user);
+            $scope.message = 'Thanks, ' + $scope.user.first + ', we added you!';
+        };
+
+        /**
+         * 获取验证码（倒计时）
+         */
+        $scope.getValidateCode = function () {
+            $http.post('/wx/getValidateCode.do', {mobile: mobile}).then(function (re) {
+                // success
+                console.log('success', re);
+
+                $scope.msg = '验证码已发送！';
+
+                $scope.countDown = 60;
+                var timer = setInterval(function () {
+                    $scope.countDown--;
+                    $scope.$apply();
+                    if ($scope.countDown <= 0) {
+                        clearInterval(timer);
+                        $scope.smsVerifyBtnDisabled = false;
+                        $scope.$apply();
+                    }
+                }, 1000);
+            }, function (re) {
+                // error
+                console.log('err', re)
+            });
+        }
+    });
